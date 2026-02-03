8d5e80b83cf52ba37d9f8f15745ef995a7a80a6f
andreamurari <murariandrea00@gmail.com>
2026-02-03 15:48:07 +0100

    Add support for updating "Durata_Aste" Google Sheets

--- fc_to_sb_to_gs_ETL.py
+++ fc_to_sb_to_gs_ETL.py
@@ -32,6 +32,7 @@ SUPABASE_PASSWORD = os.environ.get("SUPABASE_PASSWORD")
 SUPABASE_TABLE = os.environ.get("SUPABASE_TABLE", "giocatore")
 SUPABASE_TABLE_CREDITI = os.environ.get("SUPABASE_TABLE_CREDITI", "squadra")
 SUPABASE_TABLE_MOVIMENTI = os.environ.get("SUPABASE_TABLE_MOVIMENTI", "movimenti_squadra")
+SUPABASE_TABLE_ASTE = os.environ.get("SUPABASE_TABLE_ASTE", "asta")
 
 FANTACALCIO_USERNAME = os.environ.get("FANTACALCIO_USERNAME", "mura88")
 FANTACALCIO_PASSWORD = os.environ.get("FANTACALCIO_PASSWORD")  # <- SECRET
@@ -374,6 +375,21 @@ if __name__ == '__main__':
     worksheet_movimenti = spreadsheet.worksheet("Mercato")    
     worksheet_movimenti.clear()
     set_with_dataframe(worksheet_movimenti, sbm)
+    print("✅ Movimenti mercato aggiornati nel Google Sheet.")
+    
+    # === Aggiorna movimenti mercato nel foglio Google Sheet ===
+    print("⬆️ Aggiornamento tabella aste in Google Sheet...")
+    conn = psycopg2.connect(
+        host=SUPABASE_HOST,
+        port=SUPABASE_PORT,
+        dbname=SUPABASE_DB,
+        user=SUPABASE_USER,
+        password=SUPABASE_PASSWORD
+    )
+    aste = pd.read_sql(f"SELECT * FROM {SUPABASE_TABLE_ASTE};", conn)
+    worksheet_aste = spreadsheet.worksheet("Durata_Aste")    
+    worksheet_aste.clear()
+    set_with_dataframe(worksheet_aste, aste)
     print("✅ Movimenti mercato aggiornati nel Google Sheet.")
     
     print("🎉 ETL completato con successo!")
